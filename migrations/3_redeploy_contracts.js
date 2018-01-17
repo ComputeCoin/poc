282585e2be0850506785da88dd1827f1efc33cc3
Sam Rahman <mohammadsr@gmail.com>
2018-01-17 18:26:25 -0500

    working contract and job creation

--- migrations/3_redeploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Compute = artifacts.require("Compute");
-var JobContract = artifacts.require("JobContract");
-
-module.exports = function(deployer) {
-  deployer.deploy(Compute);
-  deployer.deploy(JobContract);
-};

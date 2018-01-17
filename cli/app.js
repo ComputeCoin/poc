b6fb1369cdc9a4f391b8bc812c826b29d29a5c33
Sam Rahman <mohammadsr@gmail.com>
2018-01-17 18:26:25 -0500

    working contract and job creation

--- cli/app.js
+++ cli/app.js
@@ -6,7 +6,7 @@ if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
   // set the provider you want from Web3.providers
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
 console.log(Web3);
@@ -14,6 +14,8 @@ const ComputeContractJSON = require('../build/contracts/Compute.json');
 const JobContractJSON = require('../build/contracts/JobContract.json');
 
 console.log("contract jsons loaded");
+const singletonAddress = "0x4400bd1df1c33743bc09cafaabd1af4b031c4465";
+//ComputeContractJSON.address = singletonAddress;
 const computeContract = TruffleContract(ComputeContractJSON);
 
 console.log("compute contract loaded");
@@ -24,16 +26,30 @@ console.log("compute contract provider attached");
 
 computeContract.deployed()
   .then(function(computeContractInstance) {
-    console.log("compute contract deployed and instance created");
+    console.log("compute contract deployed and instance created", computeContractInstance);
     computeContractInstance.totalJobs().then(function(response){
       console.log("totalJobs=", response);
     });
-    computeContractInstance.createJob.call(2, "imageid1").then(function(response){
-      console.log("job created", response);
-    })
-    .catch(function(err) {
-      console.log(err);
+
+    web3.eth.getAccounts(function(error, accounts) {
+      console.log(error, accounts);
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[1];
+      balance = web3.eth.getBalance(account);
+      console.log("balance", balance);
+      console.log("the currenct account is", account);
+      computeContractInstance.createJob(2, "i", {from:account, gas:1000000}).then(function(response){
+        console.log("job created", response);
+      })
+      .catch(function(err) {
+        console.log("error creating job", err);
+      });
+
     });
+
   })
   .catch(function(err) {
     console.log(err.message);

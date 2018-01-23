05a4d800c3665aa1b9f35457cc6c9cb6cc671c2d
Sam Rahman <mohammadsr@gmail.com>
2018-01-23 17:00:42 -0500

    cli enhancements

--- cli/jobsubmitter.js
+++ cli/jobsubmitter.js
@@ -3,7 +3,8 @@ const Web3Personal = require('web3-shh');
 const async = require("async");
 const WebsocketProvider = require("web3-providers-ws");
 const identities = require("./identities.json");
-
+const fs = require("fs");
+const jsyaml = require("js-yaml");
 const writeYaml = require('write-yaml');
 const { exec } = require('child_process');
 
@@ -16,125 +17,102 @@ if (typeof web3 !== 'undefined') {
 
 var shh = new Web3Personal('ws://127.0.0.1:8546');
 
-var _imagename = 'centos:7';
-var _ninstances = '2';
-var _ncpus = '0.1';
-var _memory = '50M';
-var _ports = '80:80';
+shh.net.getId(function(err, id){
+  console.log("peerid", err, id);
+});
+
 var _swarmCommandToken = {};
 
-var jobDescription = {
-  "type":"job",
-  "numInstances":2,
-  "imageLocation":"ipfshash1",
-  "dockerSwarmJoinCommand": _swarmCommandToken.cmd,
-  "dockerSwarmToken": _swarmCommandToken.token
+function saveDockerImageLocally(imageName, callback) {
+
+	// execute
+	// docker save imageName -o cluster-img.tar
+	var tarfile = 'cpucoin.' + imageName + '.tar';
+	let command = 'docker save ' + imageName + ' -o ' + tarfile;
+	console.log('Executing command, ', command);
+
+	exec(command, function(err, stdout, stderr){
+    console.log(err, stdout, stderr);
+    callback(err, tarfile);
+  });
 }
 
-function send() {
-  var sent = shh.post({
-       symKeyID: identities.symKeyID, // encrypts using the sym key ID
-       sig: identities.signature, // signs the message using the keyPair ID
-       ttl: 10,
-       topic:  '0x12345678',
-       payload:  web3.fromAscii(JSON.stringify(jobDescription)),
-       powTime: 1,
-       powTarget: 0.2
-   }, function(err, response){
-   });
-
-   setTimeout(send, 10000);
+function pushDockerImageToIPFS(localImageTarFile, callback) {
+  command = "ipfs add ./" + localImageTarFile;
+  exec(command, function(err, stdout, stderr){
+    console.log(err, stdout, stderr);
+    //grab ipfs hash
+    var hash = stdout.split(" ")[1]
+    callback(err, hash);
+  });
 }
 
-var isDependancyInstalled = () => {
-	// execute shell script to check if docker and IPFS
-	// is installed
-};
-
-// Write docker-compose.yml for launching docker swarm
-//
-function writeDockerCompose(imagename, ninstances, ncpus, mem, clusterports) {
-		// write docker-compose.yml
-		let ncpusStr = '/"' + ncpus + '/" \n';
-		let portStr = '/"' + clusterports + '/" \n';
-		let composeData = {
-			version:'/"3/"',
-			services:[
-					web:[
-						image: imagename,
-						deploy:[
-							replicas:ninstances,
-							resources:[
-								limits:[
-									cpus:ncpusStr,
-									memory:mem
-								]
-							],
-							restart_policy:[
-								condition:'on-failure'
-							]
-						],
-						ports:[
-							portStr
-						],
-						networks:[
-							'webnet'
-						]
-					]
-			],
-			networks:[
-				webent:' '
-			]
-		};
-
-		writeYaml.sync('./docker-compose.yml', composeData);
-	}
+function getDockerIPFSHash(imageName, callback) {
+  saveDockerImageLocally(imageName, function(err, localTar) {
+    pushDockerImageToIPFS(localTar, callback);
+  });
 }
 
-// - Define the cluster or swarm with docker_config.yml
-// - Docker Engine is used to launch a docker cluster or swarm
-// by Job Submitter using docker-compose.yml
-// - Store the command and token returned on launching docker
-// cluster or swarm
-//
-function initializeSwarm() {
-	writeDockerCompose(_imagename, _ninstances, _ncpus, _mem, _clusterports);
+function sendJob(dockerComposeFile, callback) {
+  var contents = fs.readFileSync(dockerComposeFile, "utf-8");
+  var doc = jsyaml.safeLoad(contents);
 
-	let command = 'docker swarm init');
-	exec(command, (err, stdout, stdin) {
-		if (err) {
-			console.log('An error occured executing ', command);
-			console.log(err);
-		}
+  //iterate over services, grab images, store them, add image hashes
+  var services = Object.keys(doc.services);
+  async.eachSeries(services, function(serviceKey, done){
+      console.log(serviceKey);
+      var service = doc.services[serviceKey];
+      getDockerIPFSHash(service.image, function(err, hash) {
+        service.imageIPFS = hash;
+        done();
+      });
+  },
+  function(err) {
+    var yaml = jsyaml.safeDump(doc);
+    console.log(yaml);
 
-		console.log('Swarm initialized');
-		getSwarmCommandToken(stdout);
-	});
+    var sent = shh.post({
+         symKeyID: identities.symKeyID, // encrypts using the sym key ID
+         sig: identities.signature, // signs the message using the keyPair ID
+         ttl: 10,
+         topic:  '0x12345678',
+         payload:  yaml,
+         powTime: 1,
+         powTarget: 0.2
+     }, function(err, response){
+     });
+  }
+  );
+
+
+   //
+   //
+   // //start listening for results from scheduler
+   // shh.subscribe("messages", {
+   //   symKeyID: identities.symKeyID,
+   //   topics: ['0x12345678'],
+   //   ttl:10,
+   //   minPow: 0
+   // }, function(err, message, subscription) {
+   //   if(message) {
+   //     console.log(JSON.parse(web3.toAscii(message.payload)));
+   //   }
+   //   //console.log(err, message);
+   // });
+
 }
 
-function getSwarmCommandToken(data) {
-	// Perform regex on swarm init stdout to get command and token
-	let swarmJoinCmd = stdout.split("\n")[2];
-	let swarmToken = swarmJoinCmd.split(" ")[4].trim();
+sendJob("./docker-compose.yml", function(err){
 
-	let cmdtoken = {cmd: swarmJoinCmd, token: swarmToken};
+});
 
-	return cmdtoken;
-}
-
-_swarmCommandToken = initializeSwarm();
-
-
-function setSelfAsSwarmManager() {
-	let command = 'docker swarm join-token manager');
-	exec(command, (err, stdout, stdin) {
-		if(err) {
-			console.log('An error occured executing ', command);
-			console.log(err);
-		}
-
-		console.log(stdout);
-	});
-}
-
-send();
+// //sendJob("./docker-compose.yml");
+// saveDockerImageLocally("hello-world", function(err, filename) {
+//   console.log(err, filename);
+//   pushDockerImageToIPFS(filename, function(err, hash) {
+//     console.log(err, hash);
+//     sendJob("./docker-compose.yml", hash, function(err){
+//       console.log(err);
+//     });
+//   })
+// });

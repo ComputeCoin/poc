a9596e565885ed16a41c5f1304d947294079a276
Sam Rahman <mohammadsr@gmail.com>
2018-02-13 18:40:32 -0500

    second version with cli

--- testnetwork/init.sh
+++ testnetwork/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-geth --identity "ComputeCoinTestNetwork" --wsorigins "*" --rpc  --shh --rpcport "8545" --ws --wsport "8546" --rpccorsdomain "*" --datadir "/poc/testnetwork/data" --port "30303" --nodiscover --rpcapi "db,eth,net,web3,shh,personal,admin" --networkid 1999 init /poc/testnetwork/CustomGenesis.json
+geth --identity "ComputeCoinTestNetwork" --wsorigins "*" --rpc  --shh --rpcport "8545" --ws --wsport "8546" --rpccorsdomain "*" --datadir "./data" --port "30303" --nodiscover --rpcapi "db,eth,net,web3,shh,personal,admin" --networkid 1999 init ./CustomGenesis.json
